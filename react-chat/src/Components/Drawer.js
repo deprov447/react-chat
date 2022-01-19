1f9f32b701d9c45200057afd34029ccf8be96604
Anubhav Choudhary <ac.10edu@gmail.com>
2022-01-19 17:08:41 +0530

    Sidebar
    Signed-off-by: Anubhav Choudhary <ac.10edu@gmail.com>

---
+++ react-chat/src/Components/Drawer.js
@@ -0,0 +1,38 @@
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+} from "react-pro-sidebar";
+import { BsChatDots, BsGrid1X2 } from "react-icons/bs";
+import { BiExit } from "react-icons/bi";
+import { AiOutlineHome, AiOutlineSetting } from "react-icons/ai";
+import "react-pro-sidebar/dist/css/styles.css";
+import "./Drawer.css";
+
+const Drawer = () => {
+  return (
+    <ProSidebar collapsed>
+      <SidebarHeader>
+        <BsGrid1X2 />
+      </SidebarHeader>
+      <Menu iconShape="square">
+        <MenuItem>
+          <AiOutlineHome />
+        </MenuItem>
+        <MenuItem>
+          <BsChatDots />
+        </MenuItem>
+        <MenuItem>
+          <AiOutlineSetting />
+        </MenuItem>
+        <SidebarFooter>
+          <BiExit />
+        </SidebarFooter>
+      </Menu>
+    </ProSidebar>
+  );
+};
+
+export default Drawer;
